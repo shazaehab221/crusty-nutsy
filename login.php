3a6f3d8e2f5818a57be9a5ce8c54d62bb57ddf93
Your Name <shazaehab310@gmail.com>
2025-04-24 22:53:37 +0200

    change in title

--- login.php
+++ login.php
@@ -73,7 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Slide Navbar</title>
+	<title>login&sign up</title>
 	<link rel="stylesheet" type="text/css" href="slide navbar style.css">
 <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="test1.css">
